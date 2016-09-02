a4c2ad5e55397871e0f8d32c7019e376837021c2
keerthidurai <keerthikadharmadurai@gmail.com>
2016-09-02 22:29:26 +0530

    Create Natura.java

---
+++ Natura.java
@@ -0,0 +1,14 @@
+class Nature
+{
+    public static void main(String args[])
+    {
+        int i=0,sum=0;
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(i=0;i<=n;i++)
+        {
+           sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
